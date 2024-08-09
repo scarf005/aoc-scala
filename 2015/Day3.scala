b9f451d2866e7f8393c5f92ff841becd69b17181
scarf <greenscarf005@gmail.com>
2024-08-09 21:50:51 +0900

    feat: day 3

--- 2015/Day3.scala
+++ 2015/Day3.scala
@@ -1,2 +1,40 @@
+package day3
 
-  
+import scala.io.Source.fromFile
+import scala.collection.mutable.HashSet
+
+final case class Point(x: Int, y: Int):
+  def +(p: Point) = Point(x + p.x, y + p.y)
+
+object Point:
+  def parse(c: Char) = c match
+    case '^' => Point(x = 0, y = 1)
+    case 'v' => Point(x = 0, y = -1)
+    case '<' => Point(x = -1, y = 0)
+    case '>' => Point(x = 1, y = 0)
+
+def part1(input: String) =
+  val visited = HashSet(Point(0, 0))
+  var current = Point(0, 0)
+  input.map(Point.parse).foreach { p =>
+    current += p
+    visited.add(current)
+  }
+  visited.size
+
+def part2(input: String) =
+  val visited = HashSet(Point(0, 0))
+  var santa = Point(0, 0)
+  var robo = Point(0, 0)
+  input.map(Point.parse).zipWithIndex.foreach { (p, i) =>
+    if i % 2 == 0 then santa += p else robo += p
+    visited.add(santa)
+    visited.add(robo)
+  }
+  visited.size
+
+@main def main() =
+  val input = fromFile(".cache/03.txt").mkString
+
+  println(part1(input))
+  println(part2(input))
---
+++ 2015/Day3.test.scala
@@ -0,0 +1,13 @@
+import munit.FunSuite
+import day3.{part1, part2}
+
+class Day3Tests extends FunSuite:
+  test("Part1"):
+    assertEquals(part1(">"), 2)
+    assertEquals(part1("^>v<"), 4)
+    assertEquals(part1("^v^v^v^v^v"), 2)
+
+  test("Part2"):
+    assertEquals(part2("^v"), 3)
+    assertEquals(part2("^>v<"), 3)
+    assertEquals(part2("^v^v^v^v^v"), 11)

f749bde6bf1aebfacaf8dab3a5c0b04700da8f55
scarf <greenscarf005@gmail.com>
2024-08-09 21:26:16 +0900

    style: use braceless style

--- 2015/Day1.test.scala
+++ 2015/Day1.test.scala
@@ -2,7 +2,7 @@ import munit.FunSuite
 import day1.Day1.*
 
 class Day1Tests extends FunSuite:
-  test("Part1") {
+  test("Part1"):
     assertEquals(part1("(())"), 0)
     assertEquals(part1("()()"), 0)
     assertEquals(part1("((("), 3)
@@ -12,9 +12,7 @@ class Day1Tests extends FunSuite:
     assertEquals(part1("))("), -1)
     assertEquals(part1(")))"), -3)
     assertEquals(part1(")())())"), -3)
-  }
 
-  test("Part2") {
+  test("Part2"):
     assertEquals(part2(")"), 1)
     assertEquals(part2("()())"), 5)
-  }

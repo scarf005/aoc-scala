f1bf581c5817de5b6962d55ef23af4fbed7b229f
scarf <greenscarf005@gmail.com>
2024-08-10 10:23:04 +0900

    feat: day 5

---
+++ 2015/Day5.scala
@@ -0,0 +1,29 @@
+package day5
+
+import scala.util.chaining.*
+import scala.io.Source.fromFile
+import scala.util.matching.Regex
+
+val naughtyRegex = "ab|cd|pq|xy".r
+
+def part1(s: String): Boolean =
+  val vowels = s.count("aeiou".contains)
+  val doubles = s.sliding(2).exists { xs => xs(0) == xs(1) }
+  val isNaughty = naughtyRegex.findFirstIn(s).isDefined
+
+  vowels >= 3 && doubles && !isNaughty
+
+val part2Regex = Vector(raw"(..).*\1".r, raw"(.).\1".r)
+
+def part2(s: String): Boolean =
+  def matches(x: Regex) = x.findFirstIn(s).isDefined
+
+  part2Regex.forall(matches)
+
+@main def main() =
+  val input = fromFile(".cache/05.txt").getLines().toVector
+
+  def solve(f: String => Boolean) = input.map(f).count(identity)
+
+  println(solve(part1))
+  println(solve(part2))
---
+++ 2015/Day5.test.scala
@@ -0,0 +1,16 @@
+import munit.FunSuite
+import day5.{part1, part2}
+
+class Day5Tests extends FunSuite:
+  test("Part1"):
+    assertEquals(part1("ugknbfddgicrmopn"), true)
+    assertEquals(part1("aaa"), true)
+    assertEquals(part1("jchzalrnumimnmhp"), false)
+    assertEquals(part1("haegwjzuvuyypxyu"), false)
+    assertEquals(part1("dvszwmarrgswjxmb"), false)
+
+  test("Part2"):
+    assertEquals(part2("qjhvhtzxzqqjkmpb"), true)
+    assertEquals(part2("xxyxx"), true)
+    assertEquals(part2("uurcxstgmygtbstg"), false)
+    assertEquals(part2("ieodomkazucvgmuy"), false)

9d789b3bd7f901eefebf711ea600b459ccd988a8
scarf <greenscarf005@gmail.com>
2024-08-15 20:38:56 +0900

    build: bump scrimage

--- project.scala
+++ project.scala
@@ -1,8 +1,8 @@
 //> using toolkit 0.5.0
 
-//> using dep "com.sksamuel.scrimage:scrimage-core:4.1.3"
-//> using dep "com.sksamuel.scrimage:scrimage-webp:4.1.3"
-//> using dep "com.sksamuel.scrimage:scrimage-scala_2.13:4.1.3"
+//> using dep "com.sksamuel.scrimage:scrimage-core:4.2.0"
+//> using dep "com.sksamuel.scrimage:scrimage-webp:4.2.0"
+//> using dep "com.sksamuel.scrimage:scrimage-scala_2.13:4.2.0"
 
 //> using dep "org.scala-lang.modules::scala-parallel-collections:1.0.4"
 //> using dep "org.scala-lang.modules::scala-parser-combinators:2.4.0"

2add769197ae7e9a6e430fcceff315362b2c4358
scarf <greenscarf005@gmail.com>
2024-09-03 21:21:28 +0900

    feat: day 12

---
+++ 2015/Day12.scala
@@ -0,0 +1,25 @@
+package day12
+
+import scala.io.Source.fromFile
+import scala.util.chaining.*
+
+/** recursively sum all numbers in a JSON object */
+def part1(json: ujson.Value): Int = json match
+  case ujson.Obj(obj) => obj.values.map(part1).sum
+  case ujson.Arr(arr) => arr.map(part1).sum
+  case ujson.Num(n) => n.toInt
+  case _            => 0
+
+/** recursively sum all numbers in a JSON object that does not contain the value "red" */
+def part2(json: ujson.Value): Int = json match
+  case ujson.Obj(obj) if obj.values.exists(_ == ujson.Str("red")) => 0
+  case ujson.Obj(obj) => obj.values.map(part2).sum
+  case ujson.Arr(arr) => arr.map(part2).sum
+  case ujson.Num(n) => n.toInt
+  case _            => 0
+
+@main def main =
+  val input = fromFile(".cache/12.txt").mkString.pipe(ujson.read(_))
+
+  println(part1(input))
+  println(part2(input))
---
+++ 2015/Day12.test.scala
@@ -0,0 +1,33 @@
+import munit.FunSuite
+import day12.*
+
+class Day12Tests extends FunSuite:
+  def check(fn: ujson.Value => Int, name: String)(xs: Seq[(String, Int)])(implicit
+    loc: munit.Location,
+  ): Unit =
+    xs.foreach { (input, expected) =>
+      test(s"$name: $input -> $expected"):
+        assertEquals(fn(ujson.read(input)), expected)
+    }
+
+  check(part1, "part1") {
+    Seq(
+      "[1,2,3]" -> 6,
+      """{"a":2,"b":4}""" -> 6,
+      "[[[3]]]" -> 3,
+      """{"a":{"b":4},"c":-1}""" -> 3,
+      """{"a":[-1,1]}""" -> 0,
+      """[-1,{"a":1}]""" -> 0,
+      "[]" -> 0,
+      "{}" -> 0,
+    )
+  }
+
+  check(part2, "part2") {
+    Seq(
+      "[1,2,3]" -> 6,
+      """[1,{"c":"red","b":2},3]""" -> 4,
+      """{"d":"red","e":[1,2,3,4],"f":5}""" -> 0,
+      """[1,"red",5]""" -> 6,
+    )
+  }

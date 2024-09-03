08b6f0ccaf40020b899d5289ca0df5a6406ae0c0
scarf <greenscarf005@gmail.com>
2024-09-03 21:21:37 +0900

    chore: fix warnings

--- 2015/utils.scala
+++ 2015/utils.scala
@@ -1,9 +1,11 @@
 package utils
 
+import scala.language.implicitConversions
+
 final case class Point(x: Int, y: Int):
   def +(p: Point) = Point(x + p.x, y + p.y)
 
 final case class Size(width: Int, height: Int)
 
 implicit def either2Iterable[A](e: Either[?, A]): Iterable[A] =
-  e.toOption.toIterable
+  e.toOption.toSeq

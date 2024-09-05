c30139d83f26f7f9435702daeca42836be0a11d2
scarf <greenscarf005@gmail.com>
2024-09-05 22:05:46 +0900

    refactor: use tail recursion

--- 2015/Day8.scala
+++ 2015/Day8.scala
@@ -2,23 +2,26 @@ package day8
 
 import scala.io.Source.fromFile
 import scala.util.chaining.*
+import scala.annotation.tailrec
 
 extension (s: String)
   def memoryLen = decode(s.toList).size - 2
   def encodedLen = encode(s.toList).size + 2
 
-def decode(sx: List[Char]): List[Char] = sx match
-  case '\\' :: '\\' :: xs          => '\\' +: decode(xs)
-  case '\\' :: '"' :: xs           => '"' +: decode(xs)
-  case '\\' :: 'x' :: _ :: _ :: xs => 'x' +: decode(xs)
-  case x :: xs                     => x +: decode(xs)
-  case Nil                         => Nil
+@tailrec
+def decode(ax: List[Char], acc: List[Char] = List()): List[Char] = ax match
+  case '\\' :: '\\' :: xs          => decode(xs, acc :+ '\\')
+  case '\\' :: '"' :: xs           => decode(xs, acc :+ '"')
+  case '\\' :: 'x' :: _ :: _ :: xs => decode(xs, acc :+ 'x')
+  case x :: xs                     => decode(xs, acc :+ x)
+  case Nil                         => acc
 
-def encode(sx: List[Char]): List[Char] = sx match
-  case '\\' :: xs => '\\' +: '\\' +: encode(xs)
-  case '"' :: xs  => '\\' +: '"' +: encode(xs)
-  case x :: xs    => x +: encode(xs)
-  case Nil        => Nil
+@tailrec
+def encode(ax: List[Char], acc: List[Char] = List()): List[Char] = ax match
+  case '\\' :: xs => encode(xs, acc :+ '\\' :+ '\\')
+  case '"' :: xs  => encode(xs, acc :+ '\\' :+ '"')
+  case x :: xs    => encode(xs, acc :+ x)
+  case Nil        => acc
 
 def part1(s: String) = s.size - s.memoryLen
 def part2(s: String) = s.encodedLen - s.size

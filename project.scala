a5ef741111a06434b863e3caa4ee94af7db92e06
scarf <greenscarf005@gmail.com>
2024-09-05 22:06:05 +0900

    build: bump dependencies

--- project.scala
+++ project.scala
@@ -1,10 +1,10 @@
-//> using toolkit 0.5.0
+//> using toolkit default
 
 //> using dep "com.sksamuel.scrimage:scrimage-core:4.2.0"
 //> using dep "com.sksamuel.scrimage:scrimage-webp:4.2.0"
 //> using dep "com.sksamuel.scrimage:scrimage-scala_2.13:4.2.0"
 
 //> using dep "org.scala-lang.modules::scala-parallel-collections:1.0.4"
-//> using dep "co.fs2::fs2-core:3.10.2"
+//> using dep "co.fs2::fs2-core:3.11.0"
 //> using dep "org.typelevel::cats-effect::3.5.4"
 //> using dep "org.typelevel::cats-parse::1.0.0"

8569da0731dc28b3d3da2a46043bee681c8480cf
scarf <greenscarf005@gmail.com>
2024-09-19 13:45:31 +0900

    test: day 14

---
+++ 2015/Day14.test.scala
@@ -0,0 +1,14 @@
+import scala.util.chaining.*
+import munit.FunSuite
+import day14.*
+
+class Day14Tests extends FunSuite:
+  val deers = Map(
+    "Comet" -> Deer(14, 10, 127),
+    "Dancer" -> Deer(16, 11, 162),
+  )
+  test("Part 1"):
+    assertEquals(part1(deers.values, time = 1000), 1120)
+
+  test("Part 2"):
+    assertEquals(part2(deers, time = 1000), 689)

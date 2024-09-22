b6573f919c155313693353124671a1ca6b420ff1
scarf <greenscarf005@gmail.com>
2024-09-22 22:36:28 +0900

    feat: 2022 day 4

---
+++ 2022/day04.scala
@@ -0,0 +1,29 @@
+package y2022.day04
+
+import prelude.*
+
+extension (a: Range)
+  inline infix def includes(b: Range): Boolean =
+    a.start <= b.start && b.end <= a.end
+
+  inline infix def intersects(b: Range): Boolean =
+    (a includes b) || (b includes a)
+
+object Range:
+  def unapply(s: String) =
+    s lift { case s"$a-$b" => (a.toInt to b.toInt) }
+
+def parse(s: String) =
+  s lift { case s"${Range(a)},${Range(b)}" => (a, b) }
+
+def part1(xs: Iterable[(Range, Range)]) =
+  xs.map { (a, b) => (a intersects b).toInt }.sum
+
+def part2(xs: Iterable[(Range, Range)]) =
+  xs.map { (a, b) => (a intersect b).nonEmpty.toInt }.sum
+
+@main def main() =
+  val input = fromFile(".cache/2022/04.txt").getLines.flatMap(parse).toSeq
+
+  println(part1(input))
+  println(part2(input))
---
+++ 2022/day04.test.scala
@@ -0,0 +1,25 @@
+package y2022.day04
+
+import prelude.*
+
+import munit.FunSuite
+import y2022.day04.*
+
+class Tests extends FunSuite:
+  test("range"):
+    assert((1 to 10) intersects (2 to 4))
+
+  val input = """
+    2-4,6-8
+    2-3,4-5
+    5-7,7-9
+    2-8,3-7
+    6-6,4-6
+    2-6,4-8
+  """.trim.split("\n").map(_.trim).flatMap(parse)
+
+  test("Part 1"):
+    assertEquals(part1(input), 2)
+
+  // test("Part 2"):
+  //   assertEquals(part2(res), 57600000)

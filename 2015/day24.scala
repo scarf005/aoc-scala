15f5a39ea1fbc91e63a1bb5575aeb032a82d795f
scarf <greenscarf005@gmail.com>
2024-11-09 18:12:16 +0900

    feat: 2015 day 24

---
+++ 2015/day24.scala
@@ -0,0 +1,22 @@
+package `2015`.day24
+
+import prelude.*
+
+def quantumEntanglement(xs: Iterable[Long]): Long = xs.product
+
+def solution(input: Vector[Long], group: Int) =
+  val groupWeight = input.sum / group
+  (1 to input.size).iterator
+    .map { n => input.combinations(n).filter(_.sum == groupWeight) }
+    .find(_.nonEmpty)
+    .get
+    .map(quantumEntanglement)
+    .min
+
+@main def main() =
+  val input = fromFile(".cache/2015/24.txt").getLines.map(_.toLong).toVector
+
+  val part1 = solution(input, 3)
+  println(part1)
+  val part2 = solution(input, 4)
+  println(part2)

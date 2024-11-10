c4c122734ce08570e7584ca901c16905694235bd
scarf <greenscarf005@gmail.com>
2024-11-10 17:23:49 +0900

    feat: 2015 day 25

---
+++ 2015/day25.scala
@@ -0,0 +1,12 @@
+package `2015`.day25
+
+def grid(row: Int, col: Int): Int =
+  (row + col - 1) * (row + col - 2) / 2 + col
+
+def next(n: Long): Long = n * 252533 % 33554393
+
+def at(n: Long) = Iterator.iterate(20151125L)(next).drop(n.toInt - 1).next
+
+@main def main() =
+  val target = grid(row =2981, col=3075)
+  println(at(target))

ad825ecbf39dbf7202f5cd1053f54c8e4e49604e
scarf <greenscarf005@gmail.com>
2024-12-03 23:50:14 +0900

    feat: 2024 day 3

---
+++ 2024/day03.scala
@@ -0,0 +1,27 @@
+package `2024`.day03
+
+import prelude.*
+import scala.util.matching.Regex
+
+val mul = raw"mul\((\d+),(\d+)\)".r
+val `do` = raw"do\(\)".r
+val dont = raw"don't\(\)".r
+
+def part1(text: String): Int =
+  mul.findAllMatchIn(text).sumBy { case mul(a, b) => a.toInt * b.toInt }
+
+def part2(text: String): Int = s"(${mul}|${`do`}|${dont})".r
+  .findAllMatchIn(text)
+  .foldLeft((true, 0)) { case ((on, acc), m) =>
+    m.group(1) match
+      case mul(a, b) => (on, if on then acc + a.toInt * b.toInt else acc)
+      case `do`()    => (true, acc)
+      case dont()    => (false, acc)
+  }
+  ._2
+
+@main def main() =
+  val text = readInput(this).mkString
+
+  println(part1(text))
+  println(part2(text))

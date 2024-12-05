57cf585ad4db87c3ef32ee80be6540639488a34b
scarf <greenscarf005@gmail.com>
2024-12-06 00:06:50 +0900

    refactor(2024): extract functions

--- 2024/day05.scala
+++ 2024/day05.scala
@@ -4,25 +4,31 @@ import prelude.*
 
 def middle[A](xs: IndexedSeq[A]) = xs(xs.size / 2)
 
-@main def main() =
-  val (rawRules, rawPages) = readInput(this).mkString
-    .split("\n\n")
-    .map(_.split("\n")) match { case Array(a, b) => (a, b) }
+def parseOrd(input: Iterable[String]): Ordering[Int] = input
+  .collect { case s"$a|$b" => a.toInt -> b.toInt }
+  .groupMap(_._1)(_._2)
+  .view
+  .mapValues(_.toSet)
+  .toMap
+  .withDefaultValue(Set())
+  |> (deps => Ordering.fromLessThan(deps(_)(_)))
 
-  given ord: Ordering[Int] = rawRules
-    .collect { case s"$a|$b" => a.toInt -> b.toInt }
-    .groupMap(_._1)(_._2)
-    .view
-    .mapValues(_.toSet)
-    .toMap
-    .withDefaultValue(Set())
-    |> (deps => Ordering.fromLessThan(deps(_)(_)))
+def parsePages(input: Iterable[String]): Vector[Vector[Int]] =
+  input.map(_.split(",").map(_.toInt).toVector).toVector
 
-  val pages = rawPages.map(_.split(",").map(_.toInt).toVector).toVector
-
-  val (wrongs, rights) = pages.partitionMap { page =>
-    val sort = page.sorted
-    if page == sort then Right(sort) else Left(sort)
+def parse(input: String): (Ordering[Int], Vector[Vector[Int]]) =
+  input.split("\n\n").map(_.split("\n")) match {
+    case Array(a, b) => (parseOrd(a), parsePages(b))
   }
+
+extension (xss: Iterable[IndexedSeq[Int]])
+  def partitionBySort(using deps: Ordering[Int]) = xss.partitionMap { xs =>
+    val sorted = xs.sorted
+    if xs == sorted then Right(sorted) else Left(sorted)
+  }
+
+@main def main() =
+  val (deps, pages) = readInput(this).mkString |> parse
+  val (wrongs, rights) = pages.partitionBySort(using deps)
   println(rights.sumBy(middle))
   println(wrongs.sumBy(middle))

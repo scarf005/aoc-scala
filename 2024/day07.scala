b97ac747b49aee339c5a962a721a51b61daaa5ee
scarf <greenscarf005@gmail.com>
2024-12-07 23:27:37 +0900

    feat(2024): day 07

---
+++ 2024/day07.scala
@@ -0,0 +1,33 @@
+package `2024`.day07
+
+import prelude.*
+import scala.annotation.tailrec
+
+def digits(x: Long) = math.log10(x.toDouble).toInt + 1
+
+extension (a: Long)
+  inline def ||(b: Long) = (a * math.pow(10, digits(b)) + b).toLong
+
+extension [C <: Iterable, A](xs: C[A])
+  def orEmpty[B](f: C[A] => B)(els: => B) = if (xs.isEmpty) els else f(xs)
+
+type Op = Long => Long => List[Long]
+
+def getCombo(xs: List[Long], next: Op) =
+  @tailrec def go(xs: List[Long], acc: List[Long] = List()): List[Long] =
+    xs match
+      case Nil      => acc
+      case x :: Nil => acc.flatMap(next(x))
+      case x :: ys  => go(ys, acc.orEmpty(_.flatMap(next(x)))(List(x)))
+  go(xs)
+
+def solve(xss: Vector[(Long, List[Long])], op: Op) =
+  xss.filter((target, xs) => getCombo(xs, op).exists(_ == target)).map(_._1).sum
+
+@main def main() =
+  val input = readInput(this).getLines.collect { case s"$target: $xs" =>
+    (target.toLong, xs.split(" ").map(_.toLong).toList)
+  }.toVector
+
+  println(solve(input, (x) => (a) => List(a + x, a * x)))
+  println(solve(input, (x) => (a) => List(a + x, a * x, a || x)))

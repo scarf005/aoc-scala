22d33e8c4a1631f13130c3f18788c52139d9febf
scarf <greenscarf005@gmail.com>
2024-12-10 22:43:30 +0900

    feat(2024/10): 2024 day 10

---
+++ 2024/day10.scala
@@ -0,0 +1,28 @@
+package `2024`.day10
+
+import prelude.*
+
+case class Context(grid: Grid[Int]):
+  def next(x: Pos): Vector[Pos] =
+    if grid(x) == 9 then Vector(x)
+    else
+      x.neighbours
+        .withFilter(y => grid.size(y) && grid(y) == grid(x) + 1)
+        .flatMap(next)
+
+  lazy val trails = grid.where(_ == 0).map(next)
+  lazy val part1 = trails.sumBy(_.toSet.size)
+  lazy val part2 = trails.sumBy(_.size)
+
+  override def toString = grid.repr
+    .map(xs => xs.map(x => (if x >= 0 then x else '.')).mkString)
+    .mkString("\n")
+
+@main def main() =
+  val input = readInput(this).mkString
+  val ctx = Context(Grid(input).map(_.asDigit))
+
+  println(ctx)
+  println(ctx.grid.size)
+  println(ctx.part1)
+  println(ctx.part2)

93b2e39ba5e4e8383981926ed5fa32730433885b
scarf <greenscarf005@gmail.com>
2024-12-14 15:01:20 +0900

    build: bump deps

--- project.scala
+++ project.scala
@@ -1,11 +1,10 @@
-//> using toolkit 0.6.0
-//> using option -feature -deprecation
+//> using scala "3.6.2"
+//> using options "-deprecation" "-feature"
+//> using toolkit "0.6.0"
 
-//> using dep "com.sksamuel.scrimage:scrimage-core:4.3.0"
-//> using dep "com.sksamuel.scrimage:scrimage-webp:4.3.0"
-//> using dep "com.sksamuel.scrimage:scrimage-scala_2.13:4.3.0"
+//> using dependency "com.sksamuel.scrimage:scrimage-core:4.3.0"
+//> using dependency "com.sksamuel.scrimage:scrimage-scala_2.13:4.3.0"
+//> using dependency "com.sksamuel.scrimage:scrimage-webp:4.3.0"
+//> using dependency "org.scala-lang.modules::scala-parallel-collections:1.0.4"
+//> using dependency "org.typelevel::cats-parse::1.1.0"
 
-//> using dep "org.scala-lang.modules::scala-parallel-collections:1.0.4"
-//> using dep "co.fs2::fs2-core:3.11.0"
-//> using dep "org.typelevel::cats-effect::3.5.7"
-//> using dep "org.typelevel::cats-parse::1.0.0"

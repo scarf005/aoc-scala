09847ef418d1f6c0d866ae367991aee0ccfd3436
scarf <greenscarf005@gmail.com>
2024-12-20 00:16:06 +0900

    build: bump versions

--- project.scala
+++ project.scala
@@ -1,3 +1,4 @@
+// Main
 //> using scala "3.6.2"
 //> using options "-deprecation" "-feature"
 //> using toolkit "0.6.0"
@@ -5,6 +6,5 @@
 //> using dependency "com.sksamuel.scrimage:scrimage-core:4.3.0"
 //> using dependency "com.sksamuel.scrimage:scrimage-scala_2.13:4.3.0"
 //> using dependency "com.sksamuel.scrimage:scrimage-webp:4.3.0"
-//> using dependency "org.scala-lang.modules::scala-parallel-collections:1.0.4"
+//> using dependency "org.scala-lang.modules::scala-parallel-collections:1.1.0"
 //> using dependency "org.typelevel::cats-parse::1.1.0"
-
